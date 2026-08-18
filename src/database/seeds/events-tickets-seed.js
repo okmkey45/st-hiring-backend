00facfad9da15f151ae3cd98cd4ecaacdb749dcb
Marlon Gomez <marlonsgy@gmail.com>
2026-08-18 12:29:14 +0000

    quick tweak to improve performance on database seed

--- src/database/seeds/events-tickets-seed.js
+++ src/database/seeds/events-tickets-seed.js
@@ -1,25 +1,38 @@
 const { faker } = require('@faker-js/faker')
 
 exports.seed = async function(knex) {
-  // create events and tickets for those events
-  for (let i = 0; i < 100; i++) {
-    await knex('events').insert({
+  const getFakeEvent = () => {
+    return {
       name: faker.lorem.words(),
       description: faker.lorem.paragraph(),
       date: faker.date.future(),
       location: faker.location.city(),
-    })
-  }
-  const events = await knex('events').select('id')
-  for (const event of events) {
-    // create a 500 allotment of tickets for this event
-    for (let j = 0; j < 500; j++) {
-      await knex('tickets').insert({
-        event_id: event.id,
-        status: faker.helpers.arrayElement(['available', 'sold', 'reserved']),
-        type: 'general',
-        price: 1000,
-      });
     }
+  }
+
+  const getFakeTicket = (eventId) => {
+    return {
+      event_id: eventId,
+      status: faker.helpers.arrayElement(['available', 'sold', 'reserved']),
+      type: faker.helpers.arrayElement(['general', 'vip', 'premium']),
+      price: faker.number.int({ min: 1000, max: 10000 }),
+    }
+  }
+
+  const seed = async (table, amount, getRow) => {
+    const rows = []
+    for (let i = 0; i < amount; i++) {
+      rows.push(getRow())
+    }
+
+    const seeds = await knex(table).insert(rows).returning('id')
+    return seeds.map(seed => seed.id)
+  }
+
+  // create events and tickets for those events
+  const eventsIds = await seed('events', 100, getFakeEvent)
+  // create a 500 allotment of tickets for this event
+  for (const eventId of eventsIds) {
+    await seed('tickets', 500, () => getFakeTicket(eventId))
   }
 }
